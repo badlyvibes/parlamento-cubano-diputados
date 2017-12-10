77038b1f91c214ffc2a6a2e52d748f0ec762116f
Simon Bennett <widerworld@gmail.com>
2017-12-10 01:04:11 +0000

    Initial Check-in

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,64 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'unicode_utils'
+require 'scraperwiki'
+require 'mechanize'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+def paragraph(p)
+  element = @page.at(
+    "//*[contains(@*,'post-')]/div/div[#{@div_y}]/div[#{@div_x}]/div/div/div[2]/div/p[#{p}]"
+  )
+  !element.nil? ? element.text.strip : ''
+end
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+def name
+  UnicodeUtils.titlecase(paragraph(1), :es)
+end
+
+def municipality
+  m = paragraph(2)
+  # Remove: Electo/Electa/Elegido por el / Municipio: / Nivel[...]
+  m = m.gsub(/[Mm]unicipio:/, '')
+  m = m.gsub(/[Ee]le.+?por el/, '')
+  m = m.gsub(/Nivel.+?\./, '')
+  m = m.delete('.')
+  m.strip
+end
+
+def occupation
+  paragraph(3).gsub('Ocupación: ', '').gsub(/\.$/, '')
+end
+
+def image
+  @page.at(
+    "//*[contains(@*,'post-')]/div/div[#{@div_y}]/div[#{@div_x}]/div/div/div[1]/figure/*/img"
+  ).attribute('src').value
+end
+
+def scrape
+  while name != ''
+    m = municipality
+    o = occupation
+    # Sometimes occupation details are in the municipality paragraph
+    o = m['Ocupación:'] ? m.gsub(/.+?Ocupación:/, '').gsub(/\.$/, '') : o
+    m.gsub(/Ocupación.+?$/, '')
+
+    puts ScraperWiki.save_sqlite([:name], name: name, municipality: m, occupation: o, image: image)
+
+    @div_x = @div_x + 1
+    @div_x = name == '' ? 1 : @div_x
+    @div_y = @div_x == 1 ? @div_y + 1 : @div_y
+  end
+end
+
+agent = Mechanize.new
+
+# There are 16 regions
+(1..16).each do |i|
+  start_page = agent.get('http://www.parlamentocubano.cu/index.php/diputados/')
+  anchor = "//*[contains(@*,'post-')]/div/div[1]/div/div/div/div[6]/div/ul/li[#{i}]/a"
+  # Region page
+  @page = agent.get(start_page.at(anchor).attribute('href').value)
+  @div_x = 1
+  @div_y = 1
+  scrape
+  i != 16 ? sleep(5) : nil # The server's in Cuba so can't take much load
+end
